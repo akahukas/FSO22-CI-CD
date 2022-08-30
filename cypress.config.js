41536b187e3d06165d679cd3e0a400e11754ec46
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-08-30 13:13:53 +0300

    Reverts Cypress pipeline, doubles pageLoadTimeout.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    pageLoadTimeout: 120000,
     // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
